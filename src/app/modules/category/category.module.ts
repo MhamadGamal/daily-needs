ae732fa4a11ae4de5c800199c0ebd87ad8535911
unknown <shreenabdou@gmail.com>
2019-04-07 12:28:31 +0200

    cart

--- src/app/modules/category/category.module.ts
+++ src/app/modules/category/category.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from '../../pages/category/category.component';
 import { SharedModule } from '../shared/shared.module';
 import { CategoryDetailsComponent } from '../../pages/category-details/category-details.component';
+import { CartComponent } from '../../pages/cart/cart.component';
 
 const appRoutes: Routes = [
   { path: '', component: CategoryComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
@@ -16,7 +18,7 @@ const appRoutes: Routes = [
     SharedModule
 
   ],
-  declarations: [CategoryComponent, CategoryDetailsComponent],
+  declarations: [CategoryComponent, CategoryDetailsComponent, CartComponent],
   exports: [RouterModule],
 })
 export class CategoryModule { }
---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  public pageName:string = "cart";
+  public lang:string = environment.lang;
+  public isLogged:boolean = false;
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang(this.lang); 
+    let token = localStorage.getItem('token');
+    if(token){
+        this.isLogged = true ;
+    }
+
+  }
+
+  ngOnInit() {
+  }
+
+}
